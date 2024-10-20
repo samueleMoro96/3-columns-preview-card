a9e2e56701b5a84dd5145204050288a7e77f8114
Samuele Moro <moro96.samuele@gmail.com>
2024-10-20 14:39:16 +0200

    Corrections

--- src/components/SocialList.vue
+++ src/components/SocialList.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="cardContainer_sociaList">
-    <my-social v-for="(social, index) in socials" :key="index" :label="social.label" :link="social.link" />
+    <my-social v-for="(social, index) in socials"
+      :key="index"
+      :label="social.label"
+      :link="social.link"
+    />
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent, PropType } from 'vue'
 import SocialButton from './SocialButton.vue'
 
 import '../assets/css/base.css'
 
 
-type SocialLink = {
+interface SocialLink {
   label: string,
   link: string
 }
 
-export default {
+export default defineComponent({
     name: "SocialListItem",
     components: {
         'my-social': SocialButton
     },
     props: {
         socials: {
-            type: Array as () => Array<SocialLink>,
+            type: Array as PropType<SocialLink[]>,
             default: () => []
         }
     }
